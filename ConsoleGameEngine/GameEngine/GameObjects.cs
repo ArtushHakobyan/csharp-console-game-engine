a56d788118892e807899e1880aa29d2577103374
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-17 05:56:53 -0700

    add GameObjects class

---
+++ ConsoleGameEngine/GameEngine/GameObjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class GameObjects
+    {
+        private List<GameObject> _gameObjects = new List<GameObject>();
+
+        public void Add(GameObject obj)
+        {
+            _gameObjects.Add(obj);
+        }
+
+        public bool Remove(GameObject obj)
+        {
+            return _gameObjects.Remove(obj);
+        }
+    }
+}
