50bf02673642698978961a5b7e2ab273e1a93f9f
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-17 06:03:52 -0700

    add ObjectNotFoundException class

---
+++ ConsoleGameEngine/GameEngine/ObjectNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class ObjectNotFoundException : ApplicationException
+    {
+        public ObjectNotFoundException(string msg):base(msg)
+        {
+
+        }
+
+        public ObjectNotFoundException()
+        {
+
+        }
+    }
+}
