8f3b0bd34049f88604d5f154fb14eaa48822f8e4
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-17 06:06:51 -0700

    add int and string indexers

--- ConsoleGameEngine/GameEngine/GameObjects.cs
+++ ConsoleGameEngine/GameEngine/GameObjects.cs
@@ -10,6 +10,54 @@ namespace GameEngine
     {
         private List<GameObject> _gameObjects = new List<GameObject>();
 
+        public int Count => _gameObjects.Count;
+
+        public GameObject this[string name]
+        {
+            get
+            {
+                foreach (GameObject gameObject in _gameObjects)
+                {
+                    if (gameObject.Name == name)
+                        return gameObject;
+                }
+                return null;
+            }
+            set
+            {
+                for (int i = 0; i < _gameObjects.Count; i++)
+                {
+                    if (_gameObjects[i].Name == name)
+                        _gameObjects[i] = value;
+                    return;
+                }
+                throw new ObjectNotFoundException();
+            }
+        }
+
+        public GameObject this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Count)
+                {
+                    return _gameObjects[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < Count)
+                {
+                    _gameObjects[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
         public void Add(GameObject obj)
         {
             _gameObjects.Add(obj);
